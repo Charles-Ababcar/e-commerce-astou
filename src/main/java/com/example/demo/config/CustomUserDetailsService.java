a77eeafbf5700f60dad1cff3455397afc2453143
Charles-Ababcar <ababacarcharles@gmail.com>
2025-11-17 00:40:50 +0000

    feat:jwt filter

--- src/main/java/com/example/demo/config/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/config/CustomUserDetailsService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
